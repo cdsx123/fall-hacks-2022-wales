12d58bd2ca1314c0a422526e89df53f7f216a939
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-10-15 13:23:02 -0700

    connect to api

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,78 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Button } from "@mui/material";
 import { TextField } from "@mui/material";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
-export default function Form() {
-  const [value, setValue] = React.useState(null);
-
-  useEffect(() => {
-    console.log(value.$d)
-    console.log(new Date)
-  }, [value])
+const Form = () => {
+  const { handleSubmit, control } = useForm();
+  const onSubmit = React.useCallback((input) => {
+    console.log(input)
+  },[])
 
   return (
     <form>
-      <TextField id="name" label="Assignment Name" variant="outlined"/>
-      <TextField id="weight" label="% Of Grade" variant="outlined" />
-      <TextField id="credits" label="Course Credits" variant="outlined"/>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateTimePicker
-          label="Basic example"
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-          inputFormat="DD/MM/YYYY"
-        />
-      
-      </LocalizationProvider>
-      
+      <Controller
+        name="name"
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextField
+            id="name"
+            variant="outlined"
+            label="Assignment Name"
+            required
+            autoFocus
+            onChange={onChange}
+            value={value}
+          />
+        )} />
+      <Controller
+        name="weight"
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextField
+            id="weight"
+            variant="outlined"
+            label="% Of Grade"
+            required
+            autoFocus
+            onChange={onChange}
+            value={value}
+          />
+        )} />
+      <Controller
+        name="credits"
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <TextField
+            id="credits"
+            variant="outlined"
+            label="Course Credits"
+            required
+            autoFocus
+            onChange={onChange}
+            value={value}
+          />
+        )} />
+      <Controller
+        name="date"
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateTimePicker
+              label="Basic example"
+              value={value}
+              onChange={onChange}
+              renderInput={(params) => <TextField {...params} />}
+              inputFormat="DD/MM/YYYY"
+            />
+          </LocalizationProvider>
+        )} />
+      <Button onClick={handleSubmit(onSubmit)} variant="outlined">Submit</Button>
     </form>
   )
-}
+};
+
+export default Form
