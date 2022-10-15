cf33a1593d4d83adc7e31e4d2060b4cd29ed736f
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2022-10-15 10:08:33 -0700

    more delete

--- src/App.js
+++ src/App.js
@@ -4,11 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
       </header>
     </div>
   );
