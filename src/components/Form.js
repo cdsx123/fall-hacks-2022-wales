f68f1507fe2c13851e37396a89aece6471b18871
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2022-10-15 12:58:37 -0700

    added description and difficulty fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,8 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
 export default function Form() {
-  const [value, setValue] = React.useState(null);
+  const [value, setValue] = React.useState(new Date);
+  const [difficulty, setDifficulty] = React.useState(1)
 
   useEffect(() => {
     console.log(value.$d)
@@ -17,19 +18,20 @@ export default function Form() {
       <TextField id="name" label="Assignment Name" variant="outlined"/>
       <TextField id="weight" label="% Of Grade" variant="outlined" />
       <TextField id="credits" label="Course Credits" variant="outlined"/>
+      <TextField id="difficulty" label="difficulty" variant="outlined" inputProps={{ inputMode: 'numeric', pattern: '/^[0-9][0]?$/' }} />
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DateTimePicker
-          label="Basic example"
+          id="dueDate"
+          label="Due Date"
           value={value}
           onChange={(newValue) => {
             setValue(newValue);
           }}
           renderInput={(params) => <TextField {...params} />}
-          inputFormat="DD/MM/YYYY"
         />
-      
       </LocalizationProvider>
-      
+      <TextField id="description" label="description" variant="outlined" />
+    
     </form>
   )
 }
