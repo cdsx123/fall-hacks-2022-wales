2ff9b1118d7f9d61c9b5f7a8e7ea4765a7d62b6b
TheNextLevelPlay <williamtj2001@gmail.com>
2022-10-15 18:09:27 -0700

    Finished popup for after deadline is passed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import CalendarApp from './components/Calendar/Calendar';
 import Layout from "./components/Layout";
 import Notification from "./components/Notification";
-
+import FinishTask from "./components/TaskCompletion/TaskCompletion"
 function App() {
   return (
    <>
         <Layout/>
         <Notification />
+        <FinishTask />
         <CalendarApp/>
+       
    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 
 
